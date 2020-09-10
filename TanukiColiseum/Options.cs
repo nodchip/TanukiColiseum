ff405a47ff4f01e60bd7e34a2b058554e3d8e219
nodchip <nodchip@gmail.com>
2020-09-10 16:13:28 +0900

    コマンドライン引数のパースをSystem.CommandLine.DragonFruitを用いて行うよう変更した

--- TanukiColiseum/Options.cs
+++ TanukiColiseum/Options.cs
@@ -4,159 +4,30 @@ namespace TanukiColiseum
 {
     public class Options
     {
-        public string Engine1FilePath { get; set; } = null;
-        public string Engine2FilePath { get; set; } = null;
-        public string Eval1FolderPath { get; set; } = null;
-        public string Eval2FolderPath { get; set; } = null;
-        public int NumConcurrentGames { get; set; } = 0;
-        public int NumGames { get; set; } = 0;
-        public int HashMb { get; set; } = 0;
-        public int NumBookMoves1 { get; set; } = 0;
-        public int NumBookMoves2 { get; set; } = 0;
-        public string BookFileName1 { get; set; } = "no_book";
-        public string BookFileName2 { get; set; } = "no_book";
-        public int NumBookMoves { get; set; } = 0;
-        public string SfenFilePath { get; set; } = "records_2017-05-19.sfen";
-        public int Nodes1 { get; set; } = 0;
-        public int Nodes2 { get; set; } = 0;
-        public int NumNumaNodes { get; set; } = 1;
-        public int ProgressIntervalMs { get; set; } = 60 * 1000;
-        public int NumThreads1 { get; set; } = 1;
-        public int NumThreads2 { get; set; } = 1;
-        public int BookEvalDiff1 { get; set; } = 30;
-        public int BookEvalDiff2 { get; set; } = 30;
-        public string ConsiderBookMoveCount1 { get; set; } = "false";
-        public string ConsiderBookMoveCount2 { get; set; } = "false";
-        public string IgnoreBookPly1 { get; set; } = "false";
-        public string IgnoreBookPly2 { get; set; } = "false";
-
-        public void Parse(string[] args)
-        {
-            for (int i = 0; i < args.Length; ++i)
-            {
-                switch (args[i])
-                {
-                    case "TanukiColiseum.exe":
-                        break;
-                    case "--engine1":
-                        Engine1FilePath = args[++i];
-                        break;
-                    case "--engine2":
-                        Engine2FilePath = args[++i];
-                        break;
-                    case "--eval1":
-                        Eval1FolderPath = args[++i];
-                        break;
-                    case "--eval2":
-                        Eval2FolderPath = args[++i];
-                        break;
-                    case "--num_concurrent_games":
-                        NumConcurrentGames = int.Parse(args[++i]);
-                        break;
-                    case "--num_games":
-                        NumGames = int.Parse(args[++i]);
-                        break;
-                    case "--hash":
-                        HashMb = int.Parse(args[++i]);
-                        break;
-                    case "--nodes1":
-                        Nodes1 = int.Parse(args[++i]);
-                        break;
-                    case "--nodes2":
-                        Nodes2 = int.Parse(args[++i]);
-                        break;
-                    case "--num_numa_nodes":
-                        NumNumaNodes = int.Parse(args[++i]);
-                        break;
-                    case "--num_book_moves1":
-                        NumBookMoves1 = int.Parse(args[++i]);
-                        break;
-                    case "--num_book_moves2":
-                        NumBookMoves2 = int.Parse(args[++i]);
-                        break;
-                    case "--book_file_name1":
-                        BookFileName1 = args[++i];
-                        break;
-                    case "--book_file_name2":
-                        BookFileName2 = args[++i];
-                        break;
-                    case "--num_book_moves":
-                        NumBookMoves = int.Parse(args[++i]);
-                        break;
-                    case "--sfen_file_name":
-                        SfenFilePath = args[++i];
-                        break;
-                    case "--progress_interval_ms":
-                        ProgressIntervalMs = int.Parse(args[++i]);
-                        break;
-                    case "--num_threads1":
-                        NumThreads1 = int.Parse(args[++i]);
-                        break;
-                    case "--num_threads2":
-                        NumThreads2 = int.Parse(args[++i]);
-                        break;
-                    case "--book_eval_diff1":
-                        BookEvalDiff1 = int.Parse(args[++i]);
-                        break;
-                    case "--book_eval_diff2":
-                        BookEvalDiff2 = int.Parse(args[++i]);
-                        break;
-                    case "--consider_book_move_count1":
-                        ConsiderBookMoveCount1 = args[++i];
-                        break;
-                    case "--consider_book_move_count2":
-                        ConsiderBookMoveCount2 = args[++i];
-                        break;
-                    case "--ignore_book_ply1":
-                        IgnoreBookPly1 = args[++i];
-                        break;
-                    case "--ignore_book_ply2":
-                        IgnoreBookPly2 = args[++i];
-                        break;
-                    default:
-                        throw new ArgumentException("Unexpected option: " + args[i]);
-                }
-            }
-        }
-
-        public void Validate()
-        {
-            if (Engine1FilePath == null)
-            {
-                throw new ArgumentException("--engine1 is not specified.");
-            }
-            else if (Engine2FilePath == null)
-            {
-                throw new ArgumentException("--engine2 is not specified.");
-            }
-            else if (Eval1FolderPath == null)
-            {
-                throw new ArgumentException("--eval1 is not specified.");
-            }
-            else if (Eval2FolderPath == null)
-            {
-                throw new ArgumentException("--eval2 is not specified.");
-            }
-            else if (NumConcurrentGames == 0)
-            {
-                throw new ArgumentException("--num_concurrent_games is not specified.");
-            }
-            else if (NumGames == 0)
-            {
-                throw new ArgumentException("--num_games is not specified.");
-            }
-            else if (HashMb == 0)
-            {
-                throw new ArgumentException("--hash is not specified.");
-            }
-            else if (Nodes1 == 0)
-            {
-                throw new ArgumentException("--nodes1 is not specified.");
-            }
-            else if (Nodes2 == 0)
-            {
-                throw new ArgumentException("--nodes2 is not specified.");
-            }
-        }
+        public string Engine1FilePath { get; set; }
+        public string Engine2FilePath { get; set; }
+        public string Eval1FolderPath { get; set; }
+        public string Eval2FolderPath { get; set; }
+        public int NumConcurrentGames { get; set; }
+        public int NumGames { get; set; }
+        public int HashMb { get; set; }
+        public int NumBookMoves1 { get; set; }
+        public int NumBookMoves2 { get; set; }
+        public string BookFileName1 { get; set; }
+        public string BookFileName2 { get; set; }
+        public int NumBookMoves { get; set; }
+        public string SfenFilePath { get; set; }
+        public int Nodes1 { get; set; }
+        public int Nodes2 { get; set; }
+        public int NumNumaNodes { get; set; }
+        public int ProgressIntervalMs { get; set; }
+        public int NumThreads1 { get; set; }
+        public int NumThreads2 { get; set; }
+        public int BookEvalDiff1 { get; set; }
+        public int BookEvalDiff2 { get; set; }
+        public string ConsiderBookMoveCount1 { get; set; }
+        public string ConsiderBookMoveCount2 { get; set; }
+        public string IgnoreBookPly1 { get; set; }
+        public string IgnoreBookPly2 { get; set; }
     }
 }
--- TanukiColiseum/Program.cs
+++ TanukiColiseum/Program.cs
@@ -4,19 +4,64 @@ namespace TanukiColiseum
 {
     class Program
     {
-        public void Run(string[] args)
+        static void Main(
+            string engine1FilePath,
+            string engine2FilePath,
+            string eval1FolderPath,
+            string eval2FolderPath,
+            int numConcurrentGames,
+            int numGames,
+            int hashMb,
+            int nodes1,
+            int nodes2,
+            int numBookMoves1 = 0,
+            int numBookMoves2 = 0,
+            string bookFileName1 = "no_book",
+            string bookFileName2 = "no_book",
+            int numBookMoves = 0,
+            string sfenFilePath = "records2016_10818.sfen",
+            int numNumaNodes = 1,
+            int progressIntervalMs = 60 * 60 * 1000,
+            int numThreads1 = 1,
+            int numThreads2 = 1,
+            int bookEvalDiff1 = 30,
+            int bookEvalDiff2 = 30,
+            string considerBookMoveCount1 = "false",
+            string considerBookMoveCount2 = "false",
+            string ignoreBookPly1 = "false",
+            string ignoreBookPly2 = "false")
         {
-            var options = new Options();
-            options.Parse(args);
-            options.Validate();
+            var options = new Options
+            {
+                Engine1FilePath = engine1FilePath,
+                Engine2FilePath = engine2FilePath,
+                Eval1FolderPath = eval1FolderPath,
+                Eval2FolderPath = eval2FolderPath,
+                NumConcurrentGames = numConcurrentGames,
+                NumGames = numGames,
+                HashMb = hashMb,
+                NumBookMoves1 = numBookMoves1,
+                NumBookMoves2 = numBookMoves2,
+                BookFileName1 = bookFileName1,
+                BookFileName2 = bookFileName2,
+                NumBookMoves = numBookMoves,
+                SfenFilePath = sfenFilePath,
+                Nodes1 = nodes1,
+                Nodes2 = nodes2,
+                NumNumaNodes = numNumaNodes,
+                ProgressIntervalMs = progressIntervalMs,
+                NumThreads1 = numThreads1,
+                NumThreads2 = numThreads2,
+                BookEvalDiff1 = bookEvalDiff1,
+                BookEvalDiff2 = bookEvalDiff2,
+                ConsiderBookMoveCount1 = considerBookMoveCount1,
+                ConsiderBookMoveCount2 = considerBookMoveCount2,
+                IgnoreBookPly1 = ignoreBookPly1,
+                IgnoreBookPly2 = ignoreBookPly2
+            };
+
             var cli = new Cli();
             cli.Run(options);
-        }
-
-        [STAThread]
-        static void Main(string[] args)
-        {
-            new Program().Run(args);
         }
     }
 }
