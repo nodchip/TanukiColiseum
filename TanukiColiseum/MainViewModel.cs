e1de03e62faf538f6c946e634038c8affb30ce46
nodchip <nodchip@gmail.com>
2020-10-22 12:36:09 +0900

    ViewModel実装中

---
+++ TanukiColiseum/MainViewModel.cs
@@ -0,0 +1,101 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TanukiColiseum
+{
+    public class MainViewModel
+    {
+        public ReactiveProperty<string> Engine1FilePath { get; private set; } = new ReactiveProperty<string>();
+
+        public ReactiveProperty<string> Engine2FilePath { get; private set; } = new ReactiveProperty<string>();
+
+        public ReactiveProperty<string> Eval1FolderPath { get; private set; } = new ReactiveProperty<string>("eval");
+
+        public ReactiveProperty<string> Eval2FolderPath { get; private set; } = new ReactiveProperty<string>("eval");
+
+        [RangeAttribute(1, int.MaxValue)]
+        public ReactiveProperty<int> NumConcurrentGames { get; private set; } = new ReactiveProperty<int>(1);
+
+        [RangeAttribute(1, int.MaxValue)]
+        public ReactiveProperty<int> NumGames { get; private set; } = new ReactiveProperty<int>(1000);
+
+        [RangeAttribute(1, 1048576)]
+        public ReactiveProperty<int> HashMb { get; private set; } = new ReactiveProperty<int>(16);
+
+        [RangeAttribute(0, 10000)]
+        public ReactiveProperty<int> NumBookMoves1 { get; private set; } = new ReactiveProperty<int>(16);
+
+        [RangeAttribute(0, 10000)]
+        public ReactiveProperty<int> NumBookMoves2 { get; private set; } = new ReactiveProperty<int>(16);
+
+        public ReactiveProperty<string> BookFileName1 { get; private set; } = new ReactiveProperty<string>("standard_book.db");
+
+        public ReactiveProperty<string> BookFileName2 { get; private set; } = new ReactiveProperty<string>("standard_book.db");
+
+        [RangeAttribute(0, int.MaxValue)]
+        public ReactiveProperty<int> NumBookMoves { get; private set; } = new ReactiveProperty<int>(24);
+
+        public ReactiveProperty<string> SfenFilePath { get; private set; } = new ReactiveProperty<string>("records2016_10818.sfen");
+
+        /// <summary>
+        /// 思考エンジン1に渡す思考ノード数。
+        /// <para>0が渡された場合、ノード数を指定しない。</para>
+        /// </summary>
+        [RangeAttribute(0, int.MaxValue)]
+        public ReactiveProperty<int> Nodes1 { get; private set; } = new ReactiveProperty<int>(0);
+
+        /// <summary>
+        /// 思考エンジン2に渡す思考ノード数。
+        /// <para>0が渡された場合、ノード数を指定しない。</para>
+        /// </summary>
+        [RangeAttribute(0, int.MaxValue)]
+        public ReactiveProperty<int> Nodes2 { get; private set; } = new ReactiveProperty<int>(0);
+
+        /// <summary>
+        /// 思考エンジン1に渡す思考時間。
+        /// <para>0が渡された場合、思考時間を指定しない。</para>
+        /// </summary>
+        [RangeAttribute(0, int.MaxValue)]
+        public ReactiveProperty<int> Time1 { get; private set; } = new ReactiveProperty<int>(1000);
+
+        /// <summary>
+        /// 思考エンジン2に渡す思考時間。
+        /// <para>0が渡された場合、思考時間を指定しない。</para>
+        /// </summary>
+        [RangeAttribute(0, int.MaxValue)]
+        public ReactiveProperty<int> Time2 { get; private set; } = new ReactiveProperty<int>(1000);
+
+        [RangeAttribute(1, int.MaxValue)]
+        public ReactiveProperty<int> NumNumaNodes { get; private set; } = new ReactiveProperty<int>(1);
+
+        [RangeAttribute(1, int.MaxValue)]
+        public ReactiveProperty<int> ProgressIntervalMs { get; private set; } = new ReactiveProperty<int>(1);
+
+        [RangeAttribute(1, 512)]
+        public ReactiveProperty<int> NumThreads1 { get; private set; } = new ReactiveProperty<int>(1);
+
+        [RangeAttribute(1, 512)]
+        public ReactiveProperty<int> NumThreads2 { get; private set; } = new ReactiveProperty<int>(1);
+
+        [RangeAttribute(1, 99999)]
+        public ReactiveProperty<int> BookEvalDiff1 { get; private set; } = new ReactiveProperty<int>(30);
+
+        [RangeAttribute(1, 99999)]
+        public ReactiveProperty<int> BookEvalDiff2 { get; private set; } = new ReactiveProperty<int>(30);
+
+        public ReactiveProperty<string> ConsiderBookMoveCount1 { get; private set; } = new ReactiveProperty<string>("false");
+
+        public ReactiveProperty<string> ConsiderBookMoveCount2 { get; private set; } = new ReactiveProperty<string>("false");
+
+        public ReactiveProperty<string> IgnoreBookPly1 { get; private set; } = new ReactiveProperty<string>("false");
+
+        public ReactiveProperty<string> IgnoreBookPly2 { get; private set; } = new ReactiveProperty<string>("false");
+
+        public ReactiveProperty<string> State { get; private set; } = new ReactiveProperty<string>();
+    }
+}
