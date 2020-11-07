f394099a962c74087aa14fed4c7e8a726994451e
nodchip <nodchip@gmail.com>
2020-11-07 10:49:31 +0900

    設定項目をModelに抽出した

---
+++ TanukiColiseum/MainModel.cs
@@ -0,0 +1,88 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TanukiColiseum
+{
+    /// <summary>
+    /// 設定項目を保持するモデルクラス
+    /// </summary>
+    public class MainModel : INotifyPropertyChanged
+    {
+        public ReactiveProperty<string> Engine1FilePath { get; } = new ReactiveProperty<string>("");
+
+        public ReactiveProperty<string> Engine2FilePath { get; } = new ReactiveProperty<string>("");
+
+        public ReactiveProperty<string> Eval1FolderPath { get; } = new ReactiveProperty<string>("eval");
+
+        public ReactiveProperty<string> Eval2FolderPath { get; } = new ReactiveProperty<string>("eval");
+
+        public ReactiveProperty<int> NumConcurrentGames { get; } = new ReactiveProperty<int>(1);
+
+        public ReactiveProperty<int> NumGames { get; } = new ReactiveProperty<int>(1000);
+
+        public ReactiveProperty<int> HashMb { get; } = new ReactiveProperty<int>(16);
+
+        public ReactiveProperty<int> NumBookMoves1 { get; } = new ReactiveProperty<int>(16);
+
+        public ReactiveProperty<int> NumBookMoves2 { get; } = new ReactiveProperty<int>(16);
+
+        public ReactiveProperty<string> BookFileName1 { get; } = new ReactiveProperty<string>("standard_book.db");
+
+        public ReactiveProperty<string> BookFileName2 { get; } = new ReactiveProperty<string>("standard_book.db");
+
+        public ReactiveProperty<int> NumBookMoves { get; } = new ReactiveProperty<int>(24);
+
+        public ReactiveProperty<string> SfenFilePath { get; } = new ReactiveProperty<string>("records2016_10818.sfen");
+
+        /// <summary>
+        /// 思考エンジン1に渡す思考ノード数。
+        /// <para>0が渡された場合、ノード数を指定しない。</para>
+        /// </summary>
+        public ReactiveProperty<int> Nodes1 { get; } = new ReactiveProperty<int>(0);
+
+        /// <summary>
+        /// 思考エンジン2に渡す思考ノード数。
+        /// <para>0が渡された場合、ノード数を指定しない。</para>
+        /// </summary>
+        public ReactiveProperty<int> Nodes2 { get; } = new ReactiveProperty<int>(0);
+
+        /// <summary>
+        /// 思考エンジン1に渡す思考時間。
+        /// <para>0が渡された場合、思考時間を指定しない。</para>
+        /// </summary>
+        public ReactiveProperty<int> Time1 { get; } = new ReactiveProperty<int>(1000);
+
+        /// <summary>
+        /// 思考エンジン2に渡す思考時間。
+        /// <para>0が渡された場合、思考時間を指定しない。</para>
+        /// </summary>
+        public ReactiveProperty<int> Time2 { get; } = new ReactiveProperty<int>(1000);
+
+        public ReactiveProperty<int> NumNumaNodes { get; } = new ReactiveProperty<int>(1);
+
+        public ReactiveProperty<int> ProgressIntervalMs { get; } = new ReactiveProperty<int>(1);
+
+        public ReactiveProperty<int> NumThreads1 { get; } = new ReactiveProperty<int>(1);
+
+        public ReactiveProperty<int> NumThreads2 { get; } = new ReactiveProperty<int>(1);
+
+        public ReactiveProperty<int> BookEvalDiff1 { get; } = new ReactiveProperty<int>(30);
+
+        public ReactiveProperty<int> BookEvalDiff2 { get; } = new ReactiveProperty<int>(30);
+
+        public ReactiveProperty<string> ConsiderBookMoveCount1 { get; } = new ReactiveProperty<string>("false");
+
+        public ReactiveProperty<string> ConsiderBookMoveCount2 { get; } = new ReactiveProperty<string>("false");
+
+        public ReactiveProperty<string> IgnoreBookPly1 { get; } = new ReactiveProperty<string>("false");
+
+        public ReactiveProperty<string> IgnoreBookPly2 { get; } = new ReactiveProperty<string>("false");
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
--- TanukiColiseum/MainViewModel.cs
+++ TanukiColiseum/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -11,116 +12,146 @@ namespace TanukiColiseum
 {
     public class MainViewModel
     {
-        public ReactiveProperty<string> Engine1FilePath { get; private set; } = new ReactiveProperty<string>("");
+        public ReactiveProperty<string> Engine1FilePath { get; }
 
-        public ReactiveProperty<string> Engine2FilePath { get; private set; } = new ReactiveProperty<string>("");
+        public ReactiveProperty<string> Engine2FilePath { get; }
 
-        public ReactiveProperty<string> Eval1FolderPath { get; private set; } = new ReactiveProperty<string>("eval");
+        public ReactiveProperty<string> Eval1FolderPath { get; }
 
-        public ReactiveProperty<string> Eval2FolderPath { get; private set; } = new ReactiveProperty<string>("eval");
-
-        [RangeAttribute(1, int.MaxValue)]
-        public ReactiveProperty<int> NumConcurrentGames { get; private set; } = new ReactiveProperty<int>(1);
+        public ReactiveProperty<string> Eval2FolderPath { get; }
 
         [RangeAttribute(1, int.MaxValue)]
-        public ReactiveProperty<int> NumGames { get; private set; } = new ReactiveProperty<int>(1000);
+        public ReactiveProperty<int> NumConcurrentGames { get; }
+
+        [RangeAttribute(1, int.MaxValue)]
+        public ReactiveProperty<int> NumGames { get; }
 
         [RangeAttribute(1, 1048576)]
-        public ReactiveProperty<int> HashMb { get; private set; } = new ReactiveProperty<int>(16);
+        public ReactiveProperty<int> HashMb { get; }
 
         [RangeAttribute(0, 10000)]
-        public ReactiveProperty<int> NumBookMoves1 { get; private set; } = new ReactiveProperty<int>(16);
+        public ReactiveProperty<int> NumBookMoves1 { get; }
 
         [RangeAttribute(0, 10000)]
-        public ReactiveProperty<int> NumBookMoves2 { get; private set; } = new ReactiveProperty<int>(16);
+        public ReactiveProperty<int> NumBookMoves2 { get; }
 
-        public ReactiveProperty<string> BookFileName1 { get; private set; } = new ReactiveProperty<string>("standard_book.db");
+        public ReactiveProperty<string> BookFileName1 { get; }
 
-        public ReactiveProperty<string> BookFileName2 { get; private set; } = new ReactiveProperty<string>("standard_book.db");
+        public ReactiveProperty<string> BookFileName2 { get; }
 
         [RangeAttribute(0, int.MaxValue)]
-        public ReactiveProperty<int> NumBookMoves { get; private set; } = new ReactiveProperty<int>(24);
+        public ReactiveProperty<int> NumBookMoves { get; }
 
-        public ReactiveProperty<string> SfenFilePath { get; private set; } = new ReactiveProperty<string>("records2016_10818.sfen");
+        public ReactiveProperty<string> SfenFilePath { get; }
 
         /// <summary>
         /// 思考エンジン1に渡す思考ノード数。
         /// <para>0が渡された場合、ノード数を指定しない。</para>
         /// </summary>
         [RangeAttribute(0, int.MaxValue)]
-        public ReactiveProperty<int> Nodes1 { get; private set; } = new ReactiveProperty<int>(0);
+        public ReactiveProperty<int> Nodes1 { get; }
 
         /// <summary>
         /// 思考エンジン2に渡す思考ノード数。
         /// <para>0が渡された場合、ノード数を指定しない。</para>
         /// </summary>
         [RangeAttribute(0, int.MaxValue)]
-        public ReactiveProperty<int> Nodes2 { get; private set; } = new ReactiveProperty<int>(0);
+        public ReactiveProperty<int> Nodes2 { get; }
 
         /// <summary>
         /// 思考エンジン1に渡す思考時間。
         /// <para>0が渡された場合、思考時間を指定しない。</para>
         /// </summary>
         [RangeAttribute(0, int.MaxValue)]
-        public ReactiveProperty<int> Time1 { get; private set; } = new ReactiveProperty<int>(1000);
+        public ReactiveProperty<int> Time1 { get; }
 
         /// <summary>
         /// 思考エンジン2に渡す思考時間。
         /// <para>0が渡された場合、思考時間を指定しない。</para>
         /// </summary>
         [RangeAttribute(0, int.MaxValue)]
-        public ReactiveProperty<int> Time2 { get; private set; } = new ReactiveProperty<int>(1000);
+        public ReactiveProperty<int> Time2 { get; }
 
         [RangeAttribute(1, int.MaxValue)]
-        public ReactiveProperty<int> NumNumaNodes { get; private set; } = new ReactiveProperty<int>(1);
+        public ReactiveProperty<int> NumNumaNodes { get; }
 
         [RangeAttribute(1, int.MaxValue)]
-        public ReactiveProperty<int> ProgressIntervalMs { get; private set; } = new ReactiveProperty<int>(1);
+        public ReactiveProperty<int> ProgressIntervalMs { get; }
 
         [RangeAttribute(1, 512)]
-        public ReactiveProperty<int> NumThreads1 { get; private set; } = new ReactiveProperty<int>(1);
+        public ReactiveProperty<int> NumThreads1 { get; }
 
         [RangeAttribute(1, 512)]
-        public ReactiveProperty<int> NumThreads2 { get; private set; } = new ReactiveProperty<int>(1);
+        public ReactiveProperty<int> NumThreads2 { get; }
 
         [RangeAttribute(1, 99999)]
-        public ReactiveProperty<int> BookEvalDiff1 { get; private set; } = new ReactiveProperty<int>(30);
+        public ReactiveProperty<int> BookEvalDiff1 { get; }
 
         [RangeAttribute(1, 99999)]
-        public ReactiveProperty<int> BookEvalDiff2 { get; private set; } = new ReactiveProperty<int>(30);
+        public ReactiveProperty<int> BookEvalDiff2 { get; }
 
-        public ReactiveProperty<string> ConsiderBookMoveCount1 { get; private set; } = new ReactiveProperty<string>("false");
+        public ReactiveProperty<string> ConsiderBookMoveCount1 { get; }
 
-        public ReactiveProperty<string> ConsiderBookMoveCount2 { get; private set; } = new ReactiveProperty<string>("false");
+        public ReactiveProperty<string> ConsiderBookMoveCount2 { get; }
 
-        public ReactiveProperty<string> IgnoreBookPly1 { get; private set; } = new ReactiveProperty<string>("false");
+        public ReactiveProperty<string> IgnoreBookPly1 { get; }
 
-        public ReactiveProperty<string> IgnoreBookPly2 { get; private set; } = new ReactiveProperty<string>("false");
+        public ReactiveProperty<string> IgnoreBookPly2 { get; }
 
-        public ReactiveProperty<bool> StartButtonEnabled { get; private set; } = new ReactiveProperty<bool>(true);
+        public ReactiveProperty<bool> StartButtonEnabled { get; } = new ReactiveProperty<bool>(true);
 
-        public ReactiveProperty<string> State { get; private set; } = new ReactiveProperty<string>("");
+        public ReactiveProperty<string> State { get; } = new ReactiveProperty<string>("");
 
-        public ReactiveProperty<int> ProgressBarValue { get; private set; } = new ReactiveProperty<int>();
+        public ReactiveProperty<int> ProgressBarValue { get; } = new ReactiveProperty<int>();
 
-        public ReactiveProperty<int> ProgressBarMinimum { get; private set; } = new ReactiveProperty<int>();
+        public ReactiveProperty<int> ProgressBarMinimum { get; } = new ReactiveProperty<int>();
 
-        public ReactiveProperty<int> ProgressBarMaximum { get; private set; } = new ReactiveProperty<int>();
+        public ReactiveProperty<int> ProgressBarMaximum { get; } = new ReactiveProperty<int>();
 
-        public ReactiveCommand OnSfenFilePathButton { get; private set; } = new ReactiveCommand();
+        public ReactiveCommand OnSfenFilePathButton { get; } = new ReactiveCommand();
 
-        public ReactiveCommand OnEngine1FilePathButton { get; private set; } = new ReactiveCommand();
+        public ReactiveCommand OnEngine1FilePathButton { get; } = new ReactiveCommand();
 
-        public ReactiveCommand OnEngine2FilePathButton { get; private set; } = new ReactiveCommand();
+        public ReactiveCommand OnEngine2FilePathButton { get; } = new ReactiveCommand();
 
-        public ReactiveCommand OnEval1FolderPathButton { get; private set; } = new ReactiveCommand();
+        public ReactiveCommand OnEval1FolderPathButton { get; } = new ReactiveCommand();
 
-        public ReactiveCommand OnEval2FolderPathButton { get; private set; } = new ReactiveCommand();
+        public ReactiveCommand OnEval2FolderPathButton { get; } = new ReactiveCommand();
 
-        public ReactiveCommand OnStartButton { get; private set; } = new ReactiveCommand();
+        public ReactiveCommand OnStartButton { get; } = new ReactiveCommand();
+
+        public MainModel model { get; } = new MainModel();
 
         public MainViewModel()
         {
+            Engine1FilePath = model.Engine1FilePath.ToReactivePropertyAsSynchronized(x => x.Value);
+            Engine2FilePath = model.Engine2FilePath.ToReactivePropertyAsSynchronized(x => x.Value);
+            Eval1FolderPath = model.Eval1FolderPath.ToReactivePropertyAsSynchronized(x => x.Value);
+            Eval2FolderPath = model.Eval2FolderPath.ToReactivePropertyAsSynchronized(x => x.Value);
+            NumConcurrentGames = model.NumConcurrentGames.ToReactivePropertyAsSynchronized(x => x.Value);
+            NumGames = model.NumGames.ToReactivePropertyAsSynchronized(x => x.Value);
+            HashMb = model.HashMb.ToReactivePropertyAsSynchronized(x => x.Value);
+            NumBookMoves1 = model.NumBookMoves1.ToReactivePropertyAsSynchronized(x => x.Value);
+            NumBookMoves2 = model.NumBookMoves2.ToReactivePropertyAsSynchronized(x => x.Value);
+            BookFileName1 = model.BookFileName1.ToReactivePropertyAsSynchronized(x => x.Value);
+            BookFileName2 = model.BookFileName2.ToReactivePropertyAsSynchronized(x => x.Value);
+            NumBookMoves = model.NumBookMoves.ToReactivePropertyAsSynchronized(x => x.Value);
+            SfenFilePath = model.SfenFilePath.ToReactivePropertyAsSynchronized(x => x.Value);
+            Nodes1 = model.Nodes1.ToReactivePropertyAsSynchronized(x => x.Value);
+            Nodes2 = model.Nodes2.ToReactivePropertyAsSynchronized(x => x.Value);
+            Time1 = model.Time1.ToReactivePropertyAsSynchronized(x => x.Value);
+            Time2 = model.Time2.ToReactivePropertyAsSynchronized(x => x.Value);
+            NumNumaNodes = model.NumNumaNodes.ToReactivePropertyAsSynchronized(x => x.Value);
+            ProgressIntervalMs = model.ProgressIntervalMs.ToReactivePropertyAsSynchronized(x => x.Value);
+            NumThreads1 = model.NumThreads1.ToReactivePropertyAsSynchronized(x => x.Value);
+            NumThreads2 = model.NumThreads2.ToReactivePropertyAsSynchronized(x => x.Value);
+            BookEvalDiff1 = model.BookEvalDiff1.ToReactivePropertyAsSynchronized(x => x.Value);
+            BookEvalDiff2 = model.BookEvalDiff2.ToReactivePropertyAsSynchronized(x => x.Value);
+            ConsiderBookMoveCount1 = model.ConsiderBookMoveCount1.ToReactivePropertyAsSynchronized(x => x.Value);
+            ConsiderBookMoveCount2 = model.ConsiderBookMoveCount2.ToReactivePropertyAsSynchronized(x => x.Value);
+            IgnoreBookPly1 = model.IgnoreBookPly1.ToReactivePropertyAsSynchronized(x => x.Value);
+            IgnoreBookPly2 = model.IgnoreBookPly2.ToReactivePropertyAsSynchronized(x => x.Value);
+
             OnSfenFilePathButton.Subscribe(() => SelectFilePath(SfenFilePath));
             OnEngine1FilePathButton.Subscribe(() => SelectFilePath(Engine1FilePath));
             OnEngine2FilePathButton.Subscribe(() => SelectFilePath(Engine2FilePath));
