e11dea3c7892790cf336963697313dc5b032bbfc
nodchip <nodchip@gmail.com>
2021-03-21 16:06:05 +0900

    Revert "System.CommandLine.DragonFruitをCommandLineParserに置き換えた"
    This reverts commit 62dac74f1cd55fc855f7e4eea8eeb70b6d29bb83.

--- TanukiColiseum/Program.cs
+++ TanukiColiseum/Program.cs
@@ -1,38 +1,111 @@
-﻿using CommandLine;
-using System.Threading;
+﻿using System.Threading;
 using System.Windows;
 
 namespace TanukiColiseum
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var result = Parser.Default.ParseArguments<Options>(args);
+    class Program
+    {
+        static void Main(
+            string engine1FilePath,
+            string engine2FilePath,
+            string eval1FolderPath,
+            string eval2FolderPath,
+            int numConcurrentGames,
+            int numGames,
+            int hashMb,
+            int nodes1,
+            int nodes2,
+            int nodesRandomPercent1,
+            int nodesRandomPercent2,
+            bool nodesRandomEveryMove1,
+            bool nodesRandomEveryMove2,
+            int time1,
+            int time2,
+            int byoyomi1,
+            int byoyomi2,
+            int inc1,
+            int inc2,
+            int rtime1,
+            int rtime2,
+            int numBookMoves1 = 0,
+            int numBookMoves2 = 0,
+            string bookFileName1 = "no_book",
+            string bookFileName2 = "no_book",
+            int numBookMoves = 0,
+            int maxMovesToDraw = 320,
+            string sfenFilePath = "records2016_10818.sfen",
+            int numNumaNodes = 1,
+            int progressIntervalMs = 60 * 60 * 1000,
+            int numThreads1 = 1,
+            int numThreads2 = 1,
+            int bookEvalDiff1 = 30,
+            int bookEvalDiff2 = 30,
+            string considerBookMoveCount1 = "false",
+            string considerBookMoveCount2 = "false",
+            string ignoreBookPly1 = "false",
+            string ignoreBookPly2 = "false",
+            bool gui = true)
+        {
+            var options = new Options
+            {
+                Engine1FilePath = engine1FilePath,
+                Engine2FilePath = engine2FilePath,
+                Eval1FolderPath = eval1FolderPath,
+                Eval2FolderPath = eval2FolderPath,
+                NumConcurrentGames = numConcurrentGames,
+                NumGames = numGames,
+                HashMb = hashMb,
+                NumBookMoves1 = numBookMoves1,
+                NumBookMoves2 = numBookMoves2,
+                BookFileName1 = bookFileName1,
+                BookFileName2 = bookFileName2,
+                NumBookMoves = numBookMoves,
+                MaxMovesToDraw = maxMovesToDraw,
+                SfenFilePath = sfenFilePath,
+                Nodes1 = nodes1,
+                Nodes2 = nodes2,
+                NodesRandomPercent1 = nodesRandomPercent1,
+                NodesRandomPercent2 = nodesRandomPercent2,
+                NodesRandomEveryMove1 = nodesRandomEveryMove1,
+                NodesRandomEveryMove2 = nodesRandomEveryMove2,
+                Time1 = time1,
+                Time2 = time2,
+                Byoyomi1 = byoyomi1,
+                Byoyomi2 = byoyomi2,
+                Inc1 = inc1,
+                Inc2 = inc2,
+                Rtime1 = rtime1,
+                Rtime2 = rtime2,
+                NumNumaNodes = numNumaNodes,
+                ProgressIntervalMs = progressIntervalMs,
+                NumThreads1 = numThreads1,
+                NumThreads2 = numThreads2,
+                BookEvalDiff1 = bookEvalDiff1,
+                BookEvalDiff2 = bookEvalDiff2,
+                ConsiderBookMoveCount1 = considerBookMoveCount1,
+                ConsiderBookMoveCount2 = considerBookMoveCount2,
+                IgnoreBookPly1 = ignoreBookPly1,
+                IgnoreBookPly2 = ignoreBookPly2,
+                Gui = gui,
+            };
 
-			if (result.Tag != ParserResultType.Parsed)
-			{
-				return;
-			}
-			var parsed = (Parsed<Options>)result;
-			var options = parsed.Value;
-			if (options.Gui)
-			{
-				var thread = new Thread(() =>
-				{
-					var application = new Application();
-					application.Run(new MainView());
-				});
-				thread.SetApartmentState(System.Threading.ApartmentState.STA);
-				thread.IsBackground = true; //メインスレッドが終了した場合に、動作中のスレッドも終了させる場合
-				thread.Start();
-				thread.Join();
-			}
-			else
-			{
-				var cli = new Cli();
-				cli.Run(options);
-			}
-		}
-	}
+            if (gui)
+            {
+                var thread = new Thread(() =>
+                {
+                    var application = new Application();
+                    application.Run(new MainView());
+                });
+                thread.SetApartmentState(System.Threading.ApartmentState.STA);
+                thread.IsBackground = true; //メインスレッドが終了した場合に、動作中のスレッドも終了させる場合
+                thread.Start();
+                thread.Join();
+            }
+            else
+            {
+                var cli = new Cli();
+                cli.Run(options);
+            }
+        }
+    }
 }
