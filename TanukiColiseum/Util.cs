8742b80bf108fa68299be3f8b8289b2ab9ef66bb
nodchip <nodchip@gmail.com>
2025-10-14 21:04:20 +0900

    文字列を分割する際、空文字を結果に含めないようにした。

--- TanukiColiseum/Util.cs
+++ TanukiColiseum/Util.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace TanukiColiseum
 {
-    class Util
-    {
-        public static List<string> Split(string s)
-        {
-            return new List<string>(new Regex("\\s+").Split(s));
-        }
+	class Util
+	{
+		public static List<string> Split(string s)
+		{
+			return new List<string>(new Regex("\\s+").Split(s).Where(token => !string.IsNullOrEmpty(token)));
+		}
 
-        public static string GetDateString()
-        {
-            return DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
-        }
-    }
+		public static string GetDateString()
+		{
+			return DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+		}
+	}
 }
